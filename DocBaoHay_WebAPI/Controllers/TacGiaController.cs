dc50f9dfa32255edf66978e7cc6224172a3d0c60
levanthien1812 <20521947@gm.uit.edu.vn>
2022-12-29 00:26:28 +0700

    Add follow author feature

---
+++ DocBaoHay_WebAPI/Controllers/TacGiaController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DocBaoHay_WebAPI.Controllers
+{
+    [RoutePrefix("api/tac-gia")]
+    public class TacGiaController : ApiController
+    {
+        [Route("theo-doi", Name = "ThemTheoDoiTacGia")]
+        [HttpPost]
+        public int ThemTheoDoiChuDe(int nguoiDungId, int tacGiaId)
+        {
+            Dictionary<string, object> param = new Dictionary<string, object>
+            {
+                {"NguoiDungId", nguoiDungId},
+                {"TacGiaId", tacGiaId }
+            };
+
+            int result = int.Parse(Database.Database.ExecuteCommand("ThemTheoDoiTacGia", param).ToString());
+            return result;
+        }
+    }
+}
